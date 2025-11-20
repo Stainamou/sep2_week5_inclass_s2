cc3066cfef8be43f2e311136d479c062c40a95f4
Stainamou <margussten@gmail.com>
2025-11-20 14:27:41 +0200

    commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,16 +1,28 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class App {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+public class App {
+
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!\n");
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("How many numbers to display in the loops?");
+
+            if (scanner.hasNextInt()) {
+                int loopCount = scanner.nextInt();
+
+                System.out.println("\nExecuting loop for " + loopCount + " iterations:");
+                displayNumbers(loopCount);
+            } else {
+                System.out.println("Error: Invalid input. Please enter a valid integer.");
+            }
+        }
+    }
+
+    public static void displayNumbers(int count) {
+        for (int i = 1; i <= count; i++) {
             System.out.println("i = " + i);
         }
     }
